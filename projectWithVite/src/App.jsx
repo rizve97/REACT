72a8ac54f036c912013c0003d015076191b2acd3
Rizve Ahmed <rizveahmed456@gmail.com>
2023-11-19 16:08:17 +0600

    practic

---
+++ projectWithVite/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Riju from "./riju"
+function App() {
+
+  return (
+   <>
+   
+   <Riju/>
+   <h1>Rizve Ahmed</h1>
+   <p>okay now test</p>
+   </>
+ 
+
+
+  )
+}
+
+export default App
---
+++ projectWithVite/src/riju.jsx
@@ -0,0 +1,10 @@
+function Riju(){
+  return(
+     <h3>First Function Testing</h3>
+     
+     
+  )
+
+}
+
+export default Riju
